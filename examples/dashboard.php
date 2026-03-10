88ca52e48c494b0b4e20259cc45d50d5beb9ea91
Martin Levi K.A <kettalevi@gmail.com>
2026-03-11 02:40:55 +0300

    Add dashboard.php for displaying user statistics

---
+++ examples/dashboard.php
@@ -0,0 +1,26 @@
+<?php require "../dynamic_header.php"; ?>
+
+<div style="padding:20px">
+
+<h1>Dashboard</h1>
+
+<div class="cards">
+
+<div class="card">
+<h3>Total Users</h3>
+<p>245</p>
+</div>
+
+<div class="card">
+<h3>Orders</h3>
+<p>89</p>
+</div>
+
+<div class="card">
+<h3>Revenue</h3>
+<p>$4,200</p>
+</div>
+
+</div>
+
+</div>
